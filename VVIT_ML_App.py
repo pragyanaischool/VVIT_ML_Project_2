b91b03fcbb72f6dddf6f5caefef0362a4d26ae60
pragyanaischool <75975970+pragyanaischool@users.noreply.github.com>
2026-03-17 21:46:36 +0530

    Update VVIT_ML_App.py

--- VVIT_ML_App.py
+++ VVIT_ML_App.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplot.pyplot as plt
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_squared_error
